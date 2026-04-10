285d5c598dd2b8f5ceabbcb5ab4029d205b423f2
Anandajayarajan <anandajayarajan@gmail.com>
2026-04-09 23:17:07 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function convert() {
-  let vin = parseFloat(document.getElementById("input").value);
-  let from = parseFloat(document.getElementById("from").value);
-  let to = parseFloat(document.getElementById("to").value);
-
-  let out = vin + from - to;
-
-  document.getElementById("result").innerText = out.toFixed(3);
-}
