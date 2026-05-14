33583abb398f31ad9bcd5f5116b5fe920f31878c
Anandajayarajan <anandajayarajan@gmail.com>
2026-05-14 16:38:58 -0400

    Update navbar with additional recipe categories

--- recipe-website/script.js
+++ recipe-website/script.js
@@ -3,3 +3,67 @@ function searchRecipe() {
   const value = input ? input.value.trim() : '';
   alert(value ? `Searching for: ${value}` : 'Type a recipe or ingredient first');
 }
+
+document.getElementById("navbar").innerHTML = `
+<header class="topbar">
+
+  <a class="brand" href="/webpages/recipe-website/index.html">
+    Food<span>.</span>
+  </a>
+
+  <nav class="navlinks">
+
+    <div class="nav-item">
+      <a href="/webpages/recipe-website/index.html">Home</a>
+    </div>
+
+    <div class="nav-item">
+      <a href="/webpages/recipe-website/pages/breakfast/index.html">
+        Breakfast
+      </a>
+    </div>
+
+    <div class="nav-item">
+      <a href="/webpages/recipe-website/pages/lunch/index.html">
+        Lunch
+      </a>
+
+      <div class="dropdown">
+        <a href="/webpages/recipe-website/pages/lunch/vegetarian/index.html">
+          Vegetarian
+        </a>
+
+        <a href="/webpages/recipe-website/pages/lunch/non-vegetarian/index.html">
+          Non-Vegetarian
+        </a>
+      </div>
+    </div>
+
+    <div class="nav-item">
+      <a href="/webpages/recipe-website/pages/dinner/index.html">
+        Dinner
+      </a>
+    </div>
+
+    <div class="nav-item">
+      <a href="/webpages/recipe-website/pages/snacks/index.html">
+        Snacks
+      </a>
+    </div>
+
+    <div class="nav-item">
+      <a href="/webpages/recipe-website/pages/others/index.html">
+        Others
+      </a>
+
+      <div class="dropdown">
+        <a href="/webpages/recipe-website/pages/others/soups/index.html">
+          Soups
+        </a>
+      </div>
+    </div>
+
+  </nav>
+
+ </header>
+`;
